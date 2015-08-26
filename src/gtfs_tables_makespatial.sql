32756afa2ac1aa2c193581de90de27716d5049ec
Shnn <shiva_nn@yahoo.com>
2015-08-26 13:31:50 -0400

    Updating gtfs_tables to create tables that MARTA and TriMet have more than MBTA. Editing gtfs_tables_makespatial compatible with sqlite

--- src/gtfs_tables_makespatial.sql
+++ src/gtfs_tables_makespatial.sql
@@ -1,17 +1,17 @@
 -- Add spatial support for PostGIS databases only
 
 -- Drop everything first
-DROP TABLE gtfs_shape_geoms CASCADE;
+DROP TABLE if exists gtfs_shape_geoms;
 
 BEGIN;
 -- Add the_geom column to the gtfs_stops table - a 2D point geometry
 SELECT AddGeometryColumn('gtfs_stops', 'the_geom', 4326, 'POINT', 2);
 
 -- Update the the_geom column
-UPDATE gtfs_stops SET the_geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326);
+UPDATE gtfs_stops SET the_geom = SetSRID(MakePoint(stop_lon, stop_lat), 4326);
 
 -- Create spatial index
-CREATE INDEX "gtfs_stops_the_geom_gist" ON "gtfs_stops" using gist ("the_geom" gist_geometry_ops);
+CREATE INDEX "gtfs_stops_the_geom_gist" ON "gtfs_stops" (the_geom);
 
 -- Create new table to store the shape geometries
 CREATE TABLE gtfs_shape_geoms (
@@ -23,15 +23,15 @@ SELECT AddGeometryColumn('gtfs_shape_geoms', 'the_geom', 4326, 'LINESTRING', 2);
 
 -- Populate gtfs_shape_geoms
 INSERT INTO gtfs_shape_geoms
-SELECT shape.shape_id, ST_SetSRID(ST_MakeLine(shape.the_geom), 4326) As new_geom
+SELECT shape.shape_id, SetSRID(MakeLine(shape.the_geom), 4326) As new_geom
   FROM (
-    SELECT shape_id, ST_MakePoint(shape_pt_lon, shape_pt_lat) AS the_geom
+    SELECT shape_id, MakePoint(shape_pt_lon, shape_pt_lat) AS the_geom
     FROM gtfs_shapes 
     ORDER BY shape_id, shape_pt_sequence
   ) AS shape
 GROUP BY shape.shape_id;
 
 -- Create spatial index
-CREATE INDEX "gtfs_shape_geoms_the_geom_gist" ON "gtfs_shape_geoms" using gist ("the_geom" gist_geometry_ops);
+CREATE INDEX "gtfs_shape_geoms_the_geom_gist" ON "gtfs_shape_geoms" (the_geom);
 
 COMMIT;
